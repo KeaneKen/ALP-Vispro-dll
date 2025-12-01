befea174a67e9289b2726127e29ccad111676ad5
Keane <faatihkeane@gmail.com>
2025-12-01 12:22:02 +0800

    Update the database to match the updated ERD

--- database/migrations/2025_11_28_130947_create_chat_table.php
+++ database/migrations/2025_11_28_130947_create_chat_table.php
@@ -12,21 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('chat', function (Blueprint $table) {
-            $table->id('idChat');
+            $table->string('idChat')->primary();
             $table->string('idMitra');
-            $table->unsignedBigInteger('idUser');
+            $table->string('idBumDes');
             $table->text('message');
-            $table->enum('sender_type', ['mitra', 'user']); // Siapa yang kirim pesan
-            $table->enum('status', ['sent', 'delivered', 'read'])->default('sent'); // Status
-            $table->timestamp('sent_at')->useCurrent(); // Waktu dikirim
-            $table->timestamp('read_at')->nullable(); // Waktu dibaca
+            $table->enum('sender_type', ['mitra', 'bumdes']);
+            $table->enum('status', ['sent', 'delivered', 'read'])->default('sent');
+            $table->timestamp('sent_at')->useCurrent();
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
 
             $table->foreign('idMitra')->references('idMitra')->on('mitra')->onDelete('cascade');
-            $table->foreign('idUser')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('idBumDes')->references('idBumDES')->on('bumdes')->onDelete('cascade');
         
-            // Percepat pencarian chat antara mitra dan user
-            $table->index(['idMitra', 'idUser']); 
+            $table->index(['idMitra', 'idBumDes']); 
         });
     }
 
